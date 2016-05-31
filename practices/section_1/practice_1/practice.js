7b999bf29eedf3fe60f9f498a39a8c2a61178807
qigege <ccczzz1015@163.com>
2016-05-31 23:30:22 +0800

    col_hash

---
+++ practices/section_1/practice_1/practice.js
@@ -0,0 +1,16 @@
+function collect_same_elements(collection_a, collection_b) {
+  //在这里写入代码
+  var result = [], hash = {};
+  for(var j=0,elem;(elem = collection_b[j]);j++){
+  		hash[elem]=true;
+  		}
+  for(var i=0,key;(key = collection_a[i]) != null;i++){
+
+  	if(hash[key]){
+         result.push(key);
+    }
+  }
+  alert(result);
+}
+
+module.exports = collect_same_elements;
